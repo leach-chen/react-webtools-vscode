34b0a373e69f749e1d5627729e089dcf1b7980f6
leach-chen <349763031@qq.com>
2020-12-06 10:37:57 +0800

    导入react-hot-loader包，修复引用element组件运行报错问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'element-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button>{"aaa"}</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
